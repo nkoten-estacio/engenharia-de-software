899104539a75b60c94c4e97df3e282bf7a6a6603
Ceo-Sammarco <noah.kd@gmail.com>
2025-03-25 19:32:13 -0300

    2025-1 initial commit

---
+++ provas-av.js
@@ -0,0 +1,17 @@
+
+
+let provas = {
+   Sistemas_operacionais: ""
+   ,
+   Topicos_de_big_data_em_python: "17/05/2025"
+   ,
+   Analise_de_dados: "24/05/2025"
+   ,
+   Desenvolvimento_de_software_seguro: "29/05/2025"
+   ,
+   Programacao_orientada_a_objetos_em_java: "06/06/2025"
+};
+
+
+
+
