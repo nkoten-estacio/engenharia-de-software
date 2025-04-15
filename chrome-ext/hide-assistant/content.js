c8204edc3d8a52c18f00052560cbcac65cf9133c
Ceo Sammarco <nah.kd@gmail.com>
2025-04-15 14:47:13 -0300

    @Ceo Sammarco -

---
+++ chrome-ext/hide-assistant/content.js
@@ -0,0 +1,12 @@
+
+
+const 
+   assistant = document.querySelector( "section.css-1lkavpq" )
+;
+
+if( assistant ) {
+   assistant.style.display = "none !important";
+   console.log( "assistant-o" );
+} else {
+   console.alert( "assistant.css" );
+}
