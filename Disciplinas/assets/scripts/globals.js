f5e21497f3a2f81b03ba5ee079ae275e6ad6d6fc
Ceo-Sammarco <nah.kd@gmail.com>
2025-05-05 21:41:23 -0300

    @Ceo-Sammarco - 05/05/2025

---
+++ Disciplinas/assets/scripts/globals.js
@@ -0,0 +1,27 @@
+
+
+"use strict";
+
+const 
+   _ = ( ...a ) => console.log( ...a )   
+   ,
+   $ = v => document.querySelector( v )
+   ,
+   $$ = v => document.querySelectorAll( v )
+;
+
+addEventListener( "load", () => {
+   $$( "[link]" ).forEach( link => {
+      let state;
+      if( link.getAttribute( "blank" ) ) {
+         state = "_blank";
+      } else {
+         state = "_self";
+      }
+      link.style.cssText = `cursor: pointer; text-decoration: underline; color: blue;`;
+      link.addEventListener( "click", () => {
+         window.open( link.getAttribute( "link" ), `${state}` );
+         _( `target: ${state}` );
+      } );
+   } );
+} );
