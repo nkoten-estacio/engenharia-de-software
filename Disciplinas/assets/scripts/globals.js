3fa374de0934da6548f0ca27047fbb2315410e3f
Ceo-Sammarco <nah.kd@gmail.com>
2025-05-06 22:23:45 -0300

    @Ceo-Sammarco - 06/05/2025

--- Disciplinas/assets/scripts/globals.js
+++ Disciplinas/assets/scripts/globals.js
@@ -12,16 +12,12 @@ const
 
 addEventListener( "load", () => {
    $$( "[link]" ).forEach( link => {
-      let state;
-      if( link.hasAttribute( "blank" ) ) {
-         state = "_blank";
-      } else {
-         state = "_self";
-      }
-      link.style.cssText = `cursor: pointer; text-decoration: underline; color: blue;`;
+      link.style.cssText = `cursor: pointer; text-decoration: underline; color: #2277ff;`;
       link.addEventListener( "click", () => {
-         window.open( link.getAttribute( "link" ), `${state}` );
-         _( `target: ${state}` );
+         window.open( 
+            link.getAttribute( "link" ), 
+            link.hasAttribute( "blank" ) ? "_blank" : "_self" 
+         );
       } );
    } );
 } );
