81aef754ed15c78b65fbbc3f7bcda8c4a3ca938c
Ceo-Sammarco <nah.kd@gmail.com>
2025-05-14 20:46:47 -0300

    @Ceo-Sammarco - 14/05/2025

--- js/tricks.js
+++ js/tricks.js
@@ -1,3 +1,4 @@
 
 // respostas dos simulados
 `document.querySelectorAll(".select-none").forEach(item=>{item.classList.remove("select-none")})`
+"https://hotmart.com/pt-br/marketplace/produtos/respostas-simulados-e-avaliacoes-estacio-sistemas-de-informacoes-e-analise-e-desenvolvimento-de-dados/A89528343K"
