5d5e62358a011d6c5a8bce7dfe50f6bc4eb3ca81
Ceo-Sammarco <noah.kd@gmail.com>
2025-05-28 09:09:15 -0300

    @Ceo-Sammarco - 28/05/2025

--- provas-av.js
+++ provas-av.js
@@ -3,7 +3,7 @@
 let provas = {
    Topicos_de_big_data_em_python: "17/05/2025"
    ,
-   Analise_de_dados: "24/05/2025"
+   Analise_de_dados: "09/06/2025"
    ,
    Desenvolvimento_de_software_seguro: "29/05/2025"
    ,
