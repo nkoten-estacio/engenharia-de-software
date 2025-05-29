f39f4e29e3850c31f6eb6ee6988242bd655f0e02
Ceo-Sammarco <noah.kd@gmail.com>
2025-05-29 10:32:00 -0300

    @Ceo-Sammarco - 29/05/2025

--- provas-av.js
+++ provas-av.js
@@ -3,15 +3,16 @@
 let provas = {
    Topicos_de_big_data_em_python: "17/05/2025"
    ,
-   Analise_de_dados: "09/06/2025"
-   ,
    Desenvolvimento_de_software_seguro: "29/05/2025"
    ,
    Programacao_orientada_a_objetos_em_java: "06/06/2025"
+   ,
+   Analise_de_dados: "09/06/2025"
    ,
    Sistemas_operacionais: "10/06/2025"
 };
 
 
 
-
+202309568098
+Ceo Password 00
